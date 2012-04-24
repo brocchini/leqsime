994d68090c33dd6b248e1792e7675458adcf9858
mozart.brocchini <mozart_brocchini@yahoo.com>
2012-04-24 17:50:44 -0500

    removed nested duplicate

--- leqsime/app/controllers/KeyPrefixes.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-import models.KeyPrefix;
-import play.mvc.Controller;
-
-@CRUD.For(KeyPrefix.class)
-public class KeyPrefixes extends CRUD {
-
-}
--- leqsime/app/controllers/Translations.java
+++
@@ -1,5 +0,0 @@
-package controllers;
-
-public class Translations extends CRUD {
-
-}
